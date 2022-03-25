372529068939aca09af1f9acc4ffa8d48d607743
Francisco Mesar <fmesar87@gmail.com>
2022-03-25 10:24:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+
+function  distanceFromHqInBlocks(pickup){
+    return  Math.abs(42 - pickup)
+}
+
+function distanceFromHqInFeet(pickup){
+    return Math.abs((42-pickup)*264);
+}
+
+
+function distanceTravelledInFeet(start, finish){
+    return Math.abs((start-finish)*264);
+}
+
+function   calculatesFarePrice(start, destination){
+    const feet = Math.abs((start-destination)*264)
+switch(true){
+    case feet < 400:
+        return 0
+        
+    case feet > 400 && feet < 2000:
+        return (feet - 400) * 0.02 ;
+        
+    case feet > 2000 && feet < 2500:
+        return 25
+        
+    case feet > 2500:
+        return 'cannot travel that far'
+        
+    }
+
+}
